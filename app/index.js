10f36bba7cfde1f8433aa74d320151b538273026
vamsigudala <hell@vamsikgudala@gmail.com>
2023-08-16 09:17:15 +0530

    Version1

---
+++ app/index.js
@@ -0,0 +1,40 @@
+import {View,ScrollView,SafeAreaView, Text} from 'react-native'
+import { useState } from 'react'
+import { Stack,useRouter } from 'expo-router'
+
+import { images, icons, COLORS, FONT, SIZES, SHADOWS } from '../constants'
+import { ScreenHeaderBtn,
+  Welcome,
+  Nearbyjobs,
+  Popularjobs,
+  Company,
+  JobTabs,
+  JobAbout,
+  JobFooter,
+  Specifics,
+  NearbyJobCard } from '../components'
+
+export default function index() {
+
+  const router = useRouter()
+  return (
+    <SafeAreaView style={{flex:1, backgroundColor:COLORS.lightWhite}}>
+        <Stack.Screen options={{
+          headerStyle:{backgroundColor:COLORS.lightWhite},
+          headerShadowVisible: false,
+          headerLeft: () => ( <ScreenHeaderBtn iconUrl={icons.menu} dimension='70%'/>),
+          headerRight: () => (<ScreenHeaderBtn iconUrl={images.profile} dimension='100%' />),
+          headerTitle:"",
+        }}/>
+
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <View style={{flex:1, padding: SIZES.medium}}>
+            <Welcome />
+            <Popularjobs />
+            <NearbyJobCard />
+          </View>
+
+        </ScrollView>
+    </SafeAreaView>
+  )
+}
---
+++ constants/images.js
@@ -0,0 +1,5 @@
+import profile from "../assets/images/vamsi.jpg";
+
+export default {
+  profile,
+};
